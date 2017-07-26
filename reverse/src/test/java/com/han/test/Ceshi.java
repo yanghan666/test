0cbefde17cc56dc2337c0085ec4f94923b2a9640
yanghan <614600808@qq.com>
2017-07-26 16:19:29 +0800

    再次提交

--- reverse/src/test/java/com/han/test/Ceshi.java
+++ reverse/src/test/java/com/han/test/Ceshi.java
@@ -2,13 +2,12 @@ package com.han.test;
 
 import org.junit.Test;
 
-import com.han.bean.Employee;
-import com.han.dao.EmployeeMapper;
 
 public class Ceshi {
 
 	
 	@Test
 	public void testCreat(){
+		System.out.println("hahahhahahhhaah");
 	}
 }
